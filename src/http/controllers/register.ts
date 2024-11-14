43817c3b357f0d1d2fad194e9899cef1ec81028a
netocalegari <netokalegari@gmail.com>
2024-11-14 11:23:07 -0300

    chore: create factories for authenticate and register use-cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExists } from "@/use-cases/errors/user-already-exists";
 import { RegisterUseCase } from "@/use-cases/register";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
   const resgisterBodySchema = z.object({
@@ -17,8 +18,7 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
   const { name, email, password } = resgisterBodySchema.parse(req.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
