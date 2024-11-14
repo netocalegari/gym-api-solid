a5bbebde4dab0edddf50155af6f6a25ff948cdd0
netocalegari <netokalegari@gmail.com>
2024-11-14 07:56:58 -0300

    feat: better global error handling.

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -26,7 +26,7 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
       return res.status(409).send({ message: error.message });
     }
 
-    return res.status(500).send();
+    throw error;
   }
 
   return res.status(201).send();
