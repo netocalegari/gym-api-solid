1c86e011902981d224e44e1bc7b9dd013a901a99
netocalegari <netokalegari@gmail.com>
2024-11-18 10:15:09 -0300

    fix: changing descriptions, formatting

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -6,9 +6,11 @@ export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
   async findById(id: string) {
     const gym = this.items.find((item) => item.id === id);
+
     if (!gym) {
       return null;
     }
+
     return gym;
   }
 
