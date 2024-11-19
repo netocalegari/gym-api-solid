e125c711ee6e872edae07fb6b7742bb28028ad40
netocalegari <netokalegari@gmail.com>
2024-11-19 12:00:47 -0300

    feat: gyms controllers and routes

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
+
+export async function nearby(req: FastifyRequest, res: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.body);
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase();
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return res.status(201).send({ gyms });
+}
