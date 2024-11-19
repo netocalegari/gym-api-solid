cddf2b27826413a95738b4f3ba3f6daeb30e1679
netocalegari <netokalegari@gmail.com>
2024-11-19 09:45:25 -0300

    feat: JWT auth

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,12 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
+
+export async function profile(req: FastifyRequest, res: FastifyReply) {
+  const getUserProfile = makeGetUserProfileUseCase();
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
+
+  return res.status(200).send({ user: { ...user, password_hash: undefined } });
+}
